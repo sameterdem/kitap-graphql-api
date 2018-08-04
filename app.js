491fccbbbf12606ec32a33f05374b748bf190ae1
Samet ERDEM <info@sameterdem.com>
2018-08-04 23:50:19 +0300

    Cors and bodyParser middleware added

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Express Instance
 const app = express();
+
+// Express Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 // Run the API
 app.listen(process.env.API_PORT, () => {
