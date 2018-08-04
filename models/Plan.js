3f223b4c6ead9b8eb9de2d2858079cbf1b8cda2b
Samet ERDEM <info@sameterdem.com>
2018-08-05 01:19:42 +0300

    Plan model added

---
+++ models/Plan.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const PlanSchema = new Schema({
+  boxId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  planName: {
+    type: String,
+    required: true,
+  },
+  planPrice: {
+    type: String,
+    required: true,
+  },
+  planImg: {
+    type: String,
+    default: null,
+  },
+  planShipping: {
+    type: String,
+    default: null,
+  },
+});
+
+module.exports = mongoose.model('Plan', PlanSchema);
