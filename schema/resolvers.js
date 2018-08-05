2d514126d6ac980e7e00c28facd33c2ec6091e95
Samet ERDEM <info@sameterdem.com>
2018-08-05 16:22:28 +0300

    User mutation query added

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,7 +1,48 @@
+const bcrypt = require('bcrypt');
+// Models
+const UserModel = require('../models/User');
+
 const Query = {
   hello: () => 'Hello World!',
 };
 
+const Mutation = {
+  // Create User
+  createUser: async (parent, { password, ...input }) => {
+    const user = await new UserModel({
+      ...input,
+      password: await bcrypt.hash(password, 10),
+    });
+    return user
+      .save()
+      .then(result => result)
+      .catch(err => err);
+  },
+
+  // Update User
+  updateUser: async (parent, { id, password, ...input }) => {
+    // Update password control
+    let passCheck;
+    if (password.trim()) {
+      passCheck = bcrypt.hash(password, 10);
+    } else {
+      const currentPass = await UserModel.findById({ _id: id }).then(result => result);
+      passCheck = currentPass.password;
+    }
+    return UserModel.findByIdAndUpdate(
+      id,
+      {
+        ...input,
+        password: await passCheck,
+      },
+      { new: true }
+    )
+      .then(result => result)
+      .catch(err => err);
+  },
+};
+
 module.exports = {
   Query,
+  Mutation,
 };
